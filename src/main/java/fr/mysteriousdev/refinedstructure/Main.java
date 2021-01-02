b4a654b2d51a4750e8641272269b5a3ce2f3bd96
Mysterious_Dev <mysteriousdevs@gmail.com>
2021-01-02 15:20:30 +0100

    Refined Class and package

---
+++ src/main/java/fr/mysteriousdev/refinedstructure/Main.java
@@ -0,0 +1,22 @@
+package fr.mysteriousdev.refinedstructure;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.villager.VillagerTypeHelper;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.village.VillagerType;
+import net.minecraft.world.biome.BiomeKeys;
+
+public class Main implements ModInitializer {
+
+	//ublic static final Item SHULKER_BOX = new ShulkerBoxItem(Blocks.SHULKER_BOX, (new Item.Settings()).maxCount(1).group(ItemGroup.DECORATIONS));
+	//int rawId = Registry.STRUCTURE_FEATURE.getRawId(StructureFeature.BASTION_REMNANT);
+
+	@Override
+	public void onInitialize() {
+
+
+
+
+	}
+}
